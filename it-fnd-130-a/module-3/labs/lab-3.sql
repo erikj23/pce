7771520183dd9388f1c60be073e6308100121664
erikj23 <erikj23.em@gmail.com>
2023-11-04 14:39:01 -0700

    finished lab 3

--- it-fnd-130-a/module-3/labs/lab-3.sql
+++ it-fnd-130-a/module-3/labs/lab-3.sql
@@ -1,3 +1,20 @@
 -- Select the category name, product name, and product price from both categories and products.
 -- Order the results by category name and then product name, in alphabetical order.
 -- (Hint: Join Products to Category)
+--select top 3 * from Northwind..Categories
+--select top 3 * from Northwind..Products
+select cat.CategoryName, pro.ProductName, pro.UnitPrice
+  from Northwind..Categories [cat]
+  join Northwind..Products   [pro]
+    on cat.CategoryID = pro.CategoryID
+  order by cat.CategoryName asc, pro.ProductName asc
+
+-- Select the Category Name, Product Name, and Product Price from both Categories and Products.
+-- Order the results by price highest to lowest.
+-- Show only the products that have a price from $10 to $20.
+select cat.CategoryName, pro.ProductName, pro.UnitPrice
+  from Northwind..Categories [cat]
+  join Northwind..Products   [pro]
+    on cat.CategoryID = pro.CategoryID
+  where pro.UnitPrice between 10 and 20
+  order by pro.UnitPrice desc
