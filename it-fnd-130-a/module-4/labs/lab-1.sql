d60748a5a0ec986d0a2bbf5601f47ddb04c527f1
erikj23 <erikj23.em@gmail.com>
2023-11-15 20:42:17 -0800

    fixed labs

--- it-fnd-130-a/module-4/labs/lab-1.sql
+++ it-fnd-130-a/module-4/labs/lab-1.sql
@@ -8,8 +8,8 @@
 Use Master;
 go
 
-If Exists(Select Name from SysDatabases Where Name = 'MyLabsDB_EMaldonado')
- Begin 
+If Exists (Select Name from SysDatabases Where Name = 'MyLabsDB_EMaldonado')
+ Begin
   Alter Database [MyLabsDB_EMaldonado] set Single_user With Rollback Immediate;
   Drop Database MyLabsDB_EMaldonado;
  End
@@ -21,21 +21,58 @@ go
 Use MyLabsDB_EMaldonado;
 go
 
--- Create Tables (Module 01)-- 
+-- Create Tables (Module 01)--
+drop table if exists Categories
 Create Table Categories
-([CategoryID] [int] IDENTITY(1,1) NOT NULL,[CategoryName] [nvarchar](100) NOT NULL);
+(
+  [CategoryID] [int] IDENTITY(1,1) NOT NULL,
+  [CategoryName] [nvarchar](100) NOT NULL
+);
 go
+
+drop table if exists Products
 Create Table Products
-([ProductID] [int] IDENTITY(1,1) NOT NULL 
-,[ProductName] [nvarchar](100) NOT NULL 
-,[CategoryID] [int] NULL  
-,[UnitPrice] [money] NOT NULL
+(
+  [ProductID] [int] IDENTITY(1,1) NOT NULL,
+  [ProductName] [nvarchar](100) NOT NULL,
+  [CategoryID] [int] NULL,
+  [UnitPrice] [money] NOT NULL
 );
 go
 
 
 -- Show the Current data in the Categories, Products, and Inventories Tables
 Select * from Categories;
-go
 Select * from Products;
+go
+
+
+-- Question 1: How would you add the data 'CatA' to the Categories table?
+insert into Categories (CategoryName)
+  values ('CatA')
+go
+
+-- Question 2: How would you add the data 'ProdA', for CatA, with a price of $1.99 to the Products table?
+insert into Products (CategoryID, ProductName, UnitPrice)
+  values ((select CategoryID from Categories where CategoryName = 'CatA'), 'ProdA', 1.99)
+go
+
+select * from Categories;
+select * from Products;
+go
+
+-- Question 3: How would you update the price of ProdA to $10.00 in the Products table?
+update Products set UnitPrice = 10.00
+  where ProductName = 'ProdA'
+go
+
+select * from Products;
+go
+
+-- Question 4: How would you delete the CatA data from the Categories table?
+delete from Categories
+  where CategoryName = 'CatA'
+go
+
+select * from Categories;
 go
--- it-fnd-130-a/module-4/labs/lab-2.sql
+++ it-fnd-130-a/module-4/labs/lab-2.sql
@@ -1,33 +1,137 @@
-use MyLabsDB_EMaldonado;
+--*************************************************************************--
+-- Title: Mod04 Labs Database
+-- Author: YourNameHere
+-- Desc: This file demonstrates how to process data in a database
+-- Change Log: When,Who,What
+-- 2021-01-01,YourNameHere,Created File
+--**************************************************************************--
+Use Master;
 go
 
--- Question 1: How would you add the data 'CatA' to the Categories table?
-insert into Categories (CategoryName)
-  values ('CatA')
+If Exists(Select Name from SysDatabases Where Name = 'MyLabsDB_EMaldonado')
+ Begin
+  Alter Database [MyLabsDB_EMaldonado] set Single_user With Rollback Immediate;
+  Drop Database MyLabsDB_EMaldonado;
+ End
 go
 
--- Question 2: How would you add the data 'ProdA', for CatA, with a price of $1.99 to the Products table?
-insert into Products (CategoryID, ProductName, UnitPrice)
-  values ((select CategoryID from Categories where CategoryName = 'CatA'), 'ProdA', 1.99)
+Create Database MyLabsDB_EMaldonado;
 go
 
-select * from Categories
+Use MyLabsDB_EMaldonado;
+go
+
+-- Create Tables (Module 01)--
+drop table if exists Categories
+Create Table Categories
+(
+  [CategoryID] [int] IDENTITY(1,1) NOT NULL,
+  [CategoryName] [nvarchar](100) NOT NULL
+);
+go
+
+drop table if exists Products
+Create Table Products
+(
+  [ProductID] [int] IDENTITY(1,1) NOT NULL,
+  [ProductName] [nvarchar](100) NOT NULL,
+  [CategoryID] [int] NOT NULL,
+  [UnitPrice] [money] NOT NULL
+);
+go
+
+-- Add Constraints (Module 02) --
+Alter Table Categories
+ Add Constraint pkCategories
+  Primary Key (CategoryId);
+go
+
+Alter Table Categories
+ Add Constraint ukCategories
+  Unique (CategoryName);
+go
+
+Alter Table Products
+ Add Constraint pkProducts
+  Primary Key (ProductId);
+go
+
+Alter Table Products
+ Add Constraint ukProducts
+  Unique (ProductName);
+go
+
+Alter Table Products
+ Add Constraint fkProductsToCategories
+  Foreign Key (CategoryId) References Categories(CategoryId);
+go
+
+Alter Table Products
+ Add Constraint ckProductUnitPriceZeroOrHigher
+  Check (UnitPrice >= 0);
+go
+
+-- Question 1: How would you add data to the Categories table?
+begin try
+  begin transaction 
+    insert into Categories (CategoryName)
+      values ('CatA');
+  commit transaction
+end try
+begin catch
+  if @@trancount > 0 rollback transaction
+  print 'failed insert into Categories'
+end catch
+go
+
+select * from Categories;
+go
+
+-- Question 2: How would you add data to the Products table?
+begin try
+  begin transaction
+    insert into Products (CategoryID, ProductName, UnitPrice)
+      values ((select CategoryID from Categories where CategoryName = 'CatA'), 'ProdA', 1.99)
+  commit transaction
+end try
+begin catch
+  if @@trancount > 0 rollback transaction
+  print 'failed insert into Products'
+end catch
+go
+
 select * from Products
 go
 
--- Question 3: How would you update the price of ProdA to $10.00 in the Products table?
-update Products
-  set UnitPrice = 10.00
-  where ProductName = 'ProdA'
+-- Question 3: How would you update data in the Products table?
+begin try
+  begin transaction
+    update Products set UnitPrice = 10.00 where ProductName = 'ProdA'
+  commit transaction
+end try
+begin catch
+  if @@trancount > 0 rollback transaction
+  print 'failed update Products'
+end catch
 go
 
 select * from Products
 go
 
--- Question 4: How would you delete the CatA data from the Categories table?
-delete from Categories
-  where CategoryName = 'CatA'
-go 
+-- Question 4: How would you delete data from the Categories table?
+begin try
+  begin transaction
+    delete from Products where CategoryID = (
+      select CategoryID from Categories where CategoryName = 'CatA'
+    )
+    delete from Categories where CategoryName = 'CatA'
+  commit transaction
+end try
+begin catch
+  if @@trancount > 0 rollback transaction
+  print Error_Message()
+end catch
+go
 
 select * from Categories
 go
