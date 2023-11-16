dac8c6c7f38103624e0b71753f3776b67f968514
erikj23 <erikj23.em@gmail.com>
2023-11-15 17:53:31 -0800

    finished lab 2

--- it-fnd-130-a/module-4/labs/lab-2.sql
+++ it-fnd-130-a/module-4/labs/lab-2.sql
@@ -1,4 +1,33 @@
+use MyLabsDB_EMaldonado;
+go
+
 -- Question 1: How would you add the data 'CatA' to the Categories table?
--- Question 2: How would you add the data' ProdA', for CatA, with a price of $1.99 to the Products table?
+insert into Categories (CategoryName)
+  values ('CatA')
+go
+
+-- Question 2: How would you add the data 'ProdA', for CatA, with a price of $1.99 to the Products table?
+insert into Products (CategoryID, ProductName, UnitPrice)
+  values ((select CategoryID from Categories where CategoryName = 'CatA'), 'ProdA', 1.99)
+go
+
+select * from Categories
+select * from Products
+go
+
 -- Question 3: How would you update the price of ProdA to $10.00 in the Products table?
+update Products
+  set UnitPrice = 10.00
+  where ProductName = 'ProdA'
+go
+
+select * from Products
+go
+
 -- Question 4: How would you delete the CatA data from the Categories table?
+delete from Categories
+  where CategoryName = 'CatA'
+go 
+
+select * from Categories
+go
