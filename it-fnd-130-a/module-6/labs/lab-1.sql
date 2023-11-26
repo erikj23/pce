0254dc22a0855a513035711a44747434b43d9127
erikj23 <erikj23.em@gmail.com>
2023-11-26 13:54:59 -0800

    finished lab 1

--- it-fnd-130-a/module-6/labs/lab-1.sql
+++ it-fnd-130-a/module-6/labs/lab-1.sql
@@ -25,10 +25,15 @@ go
 -- Use the IsNull() function to display null region names as the name of the customer's country?
 -- Call the view vCustomersByLocation.
 create or alter view vCustomersByLocation as
-  select [CustomerName] = ContactName, City, [Region] = ISNULL(Region, Country), Country from Northwind.dbo.Customers;
+  select
+      [CustomerName] = CompanyName,
+      [City]         = City,
+      [Region]       = ISNULL(Region, Country),
+      [Country]      = Country
+    from Northwind.dbo.Customers;
 go
 
-select * from vCustomersByLocation order by CustomerName;
+--select * from vCustomersByLocation order by CustomerName;
 go
 
 select top 3 * from Northwind.dbo.Orders;
@@ -37,4 +42,35 @@ go
 -- Question 2: How can you create a view to show a list of customer names, their locations, and the number of orders they have placed (hint: use the count() function)?
 -- Call the view vNumberOfCustomerOrdersByLocation.
 create or alter view vNumberOfCustomerOrdersByLocation as
-  select * from Northwind.dbo.Customers;
+  select
+      [CustomerName]   = CompanyName, 
+      [City]           = City,
+      [Region]         = ISNULL(Region, Country),
+      [Country]        = Country,
+      [NumberOfOrders] = (
+        select count(OrderID) from Northwind.dbo.Orders [ord] where ord.CustomerID = cus.CustomerID
+      ) 
+    from Northwind.dbo.Customers [cus];
+go
+
+--select * from vNumberOfCustomerOrdersByLocation order by CustomerName;
+go
+
+-- Question 3: How can you create a view to shows a list of customer names, their locations, and the number of orders they have placed (hint: use the count() function) on a given year (hint: use the year() function)?
+-- Call the view vNumberOfCustomerOrdersByLocationAndYears.
+create or alter view vNumberOfCustomerOrdersByLocationAndYears as
+  select
+      [CustomerName]   = CompanyName,
+      [City]           = City,
+      [Region]         = ISNULL(Region, Country),
+      [Country]        = Country,
+      [NumberOfOrders] = count(OrderID),
+      [OrderYear]      = year(OrderDate)  
+    from Northwind.dbo.Customers [cus]
+    join Northwind.dbo.Orders    [ord]
+      on ord.CustomerID = cus.CustomerID
+    group by CompanyName, City, Region, Country, year(OrderDate);
+go
+
+select * from vNumberOfCustomerOrdersByLocationAndYears order by CustomerName;
+go
